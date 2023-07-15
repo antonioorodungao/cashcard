8259d0530b716ec1054cb021b2939000e3d41619
Antonio Oro Dungao <antonioorodungao@yahoo.com>
2023-07-15 15:22:51 +0300

    InitialCommit

--- src/test/java/example/cashcard/CashCardJsonTest.java
+++ src/test/java/example/cashcard/CashCardJsonTest.java
@@ -1,2 +1,14 @@
-package example.cashcard;public class CashCardJsonTest {
+package example.cashcard;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class CashCardJsonTest {
+    @Disabled
+    @Test
+    public void myFirstTest(){
+        assertThat(1).isEqualTo(42);
+    }
 }
